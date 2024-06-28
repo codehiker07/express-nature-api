46885acb3566f1edb77611fb84381baf9790acca
Motiur Rahman <codehiker07.com>
2024-06-28 12:26:10 +0600

    api fetch from file

--- app.mjs
+++ app.mjs
@@ -1,19 +1,38 @@
+import fs from 'fs';
 import express from 'express';
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+  fs.readFileSync(`./dev-data/data/tours-simple.json`, 'utf8')
+)
+
+// app.get('/', (req, res) => {
+//   res.status(200)
+// //   .send('Hello Bro')
+//   .json({ message: 'Motiur Rahman', home: 'Rajshahi' });
+// });
+
+// app.post('/', (req, res)=>{
+//     res.status(200).send('You can post to this endpoint...');
+// })
+
+
+
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200)
-//   .send('Hello Bro')
-  .json({ message: 'Motiur Rahman', home: 'Rajshahi' });
+  .json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    },
+  });
 });
 
-app.post('/', (req, res)=>{
-    res.status(200).send('You can post to this endpoint...');
-})
-
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(port, '127.0.0.1', () => {
   console.log(`App running on port ${port}`);
 });
